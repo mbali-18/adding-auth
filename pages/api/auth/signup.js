2ae2e2f7956fda4c18c2f212cb4263d2397c8793
mbali-18 <faithfulservent18@gmail.com>
2025-07-17 18:42:18 +0200

    Adding A User Signup API Route

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,36 @@
+import { hashPassword } from "../../../lip/auth";
+import { connectToDatabase } from "../../../lip/db";
+
+async function handler(req, res) {
+  const data = req.body;
+
+  const { email, password } = data;
+
+  if (
+    !email ||
+    !email.includes("@") ||
+    !password ||
+    password.trim().length < 7
+  ) {
+    res.status(422).json({
+      message:
+        "Invalid input - password should be at least 7 characters long and email should be valid.",
+    });
+    return;
+  }
+
+  const client = await connectToDatabase();
+}
+
+const db = client.db();
+const hashedPassword = await hashPassword(password);
+
+const result = await db.collection("users").insertOne({
+  email: email,
+  password: password,
+});
+{
+  res.status(201).json({ message: "Created user!", userId: result.insertedId });
+}
+
+export default handler;
